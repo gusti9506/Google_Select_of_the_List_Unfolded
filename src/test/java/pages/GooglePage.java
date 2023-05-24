005225568225ce0dc9c9e02dd62dd56bf6814de0
Gustavo Adolfo Lopez <95432809+gusti9506@users.noreply.github.com>
2023-05-24 14:01:42 -0300

    Update GooglePage.java

--- src/test/java/pages/GooglePage.java
+++ src/test/java/pages/GooglePage.java
@@ -11,9 +11,8 @@ public class GooglePage extends BasePage {
     private String textElement = "//textarea[@value='qa automation trainee']";
     private String listBox = "//ul[@role='listbox']//li/descendant::div[@class='wM6W7d']";
 
-    public GooglePage() { //este constructor lo pide el sistema automatic. al extender de BasePage
-        super(driver);    // y tambien te obliga al "super", usa la instancia creada en BasePage cuando
-                          // usemos esta "clase"
+    public GooglePage() { 
+        super(driver);    
     }
 
     public void navigateToGoogle() {
